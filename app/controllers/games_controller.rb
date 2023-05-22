5776d6126d0428310b5a865c0fbe59986ae266a0
Bettina Tilrem <bettinatilrem@hotmail.com>
2023-05-22 17:28:08 +0800

    rails challenge 3 a bit more done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,10 +1,10 @@
 class GamesController < ApplicationController
 
-def new
-  @letters = ('a'..'z').to_a.sample(10)
-end
+  def new
+    @letters = ('a'..'z').to_a.sample(10)
+  end
 
-def score
-end
+  def score
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  get "new", to: "controller#new"
-  post "score", to: "controller#score"
+  get "new", to: "games#new"
+  post "score", to: "games#score"
 
   # Defines the root path route ("/")
   # root "articles#index"
