b7576cebc74686fde366d93836906de3179c8191
Bettina Tilrem <bettinatilrem@hotmail.com>
2023-05-22 16:42:46 +0800

    rails challenge 3 as far as i got..

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  get "new", to: "controller#new"
+  post "score", to: "controller#score"
+
   # Defines the root path route ("/")
   # root "articles#index"
 end
